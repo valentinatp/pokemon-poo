1a64828919a8d7ce91ee2835589a1e95b4f0c1dc
valentinatp <valentina.toledoporma@gmail.com>
2017-05-04 22:05:04 -0300

    Creo un constructor llamado 'pokemon' y creo las variables con los pokemones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
-function Pokemon(nombre, color){
+function Pokemon(nombre, color, poderDeAtaque){
 	this.nombre = nombre;
 	this.color = color;
 
 	this.nivelDeAmistad = 0;
 
-	this.mostrarPokemon = function(){
-		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
-	}
-
+	this.vida = 100;
+	this.poderDeAtaque = poderDeAtaque;
 }
 
-var Pikachu = new Pokemon("Pikachu", "Amarillo");
+var Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
+var Charmander = new Pokemon("Charmander", "Rojo", 20);
+
+Pikachu.atacar(Charmander);
+console.log(Charmander.vida);
