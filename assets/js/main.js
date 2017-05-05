eb0aa20ff34a4b8058e71f812db81d6cbc8491ec
valentinatp <valentina.toledoporma@gmail.com>
2017-05-04 22:41:09 -0300

    Arreglo las funciones de 'aumentar de amistad', 'ataque', creo los select para los pokemones y creo la funcion 'combatir'

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,23 @@ function Pokemon(nombre, color, poderDeAtaque){
 
 	this.aumentarAmistad = function(valor){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor;
+		return("El nivel de amistad de " + this.nombre + " es: " + this.nivelDeAmistad);
 	}
 
 	this.atacar = function(pokemon){
 		pokemon.vida = pokemon.vida - this.poderDeAtaque;
+		return("Este pokémon perdió: " + pokemon.vida + " de vida");
 	}
 
+}
+
+function combate(){
+	var dato = document.getElementById("cajita");
+	var pokemon1 = document.getElementById("pokemon1").value;
+	var pokemon2 = document.getElementById("pokemon2").value;
+	var valorAtak = prompt("Ingres un valor de ataque");
+	var atak1 = new Pokemon(pokemon1, "color", valorAtak);
+	var atak2 = new Pokemon(pokemon2, "otro color", 5);
 }
 
 var Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
