7218175c331911d49d8abd38d0b44b1db974b49c
valentinatp <valentina.toledoporma@gmail.com>
2017-05-04 22:30:04 -0300

    Creo funciones dentro del constructor para 'aumentar amistad' y para 'atacar'

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,19 @@ function Pokemon(nombre, color, poderDeAtaque){
 
 	this.vida = 100;
 	this.poderDeAtaque = poderDeAtaque;
+
+	this.mostrarPokemon = function(){
+		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
+	}
+
+	this.aumentarAmistad = function(valor){
+		this.nivelDeAmistad = this.nivelDeAmistad + valor;
+	}
+
+	this.atacar = function(pokemon){
+		pokemon.vida = pokemon.vida - this.poderDeAtaque;
+	}
+
 }
 
 var Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
